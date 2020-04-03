05c6d2c359e162df0d5ecf829cbabb2a4f2d61e6
birjitgope <59698019+birjitgope@users.noreply.github.com>
2020-04-04 00:39:40 +0530

    code update

--- INT524_code.py
+++ INT524_code.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 #preparing the dataset
-initial_dataset = pd.read_csv("DATA-FINAL.csv",usecols=[0,1,2,4,5,6,8,9,10,11,12,13,20])
-print(dataset.head())
-working_dataframe = initial_dataset[initial_dataset["MHRDName"= Bachelor of Science (Honours) (Agriculture)],initial_dataset["CourseType"=Theory]]
-print(working_dataframe.head())
-
-#choosing the model
+data = pd.read_csv("DATA-FINAL.csv",usecols=[0,1,2,4,5,6,8,9,10,11,12,13,20])
+print(data.head())
+print(data.isnull())
